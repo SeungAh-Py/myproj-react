ac7f2ac3b0a7ff2d133c63aaef6537b601d246a0
SeungAh Kim <cpuasdnwin@naver.com>
2022-01-10 08:12:32 +0900

    Reviewform 안나오는 버그 fix

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Login from 'pages/accounts/Login';
 import Components from 'pages/examples/Components';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import TopNav from 'components/TopNav';
+import ReviewForm from 'pages/reviews/ReviewForm';
 
 function App() {
   return (
     <div className="app">
       <TopNav />
       <Routes>
-        <Route path="/" element={<Navigate to="/reviews>/" />} />
+        <Route path="/" element={<Navigate to="/reviews/" />} />
         <Route path="/accounts/login/" element={<Login />} />
         <Route path="/accounts/profile/" element={<Profile />} />
         <Route path="/reviews/" element={<ReviewList />} />
+        <Route path="/reviews/new/" element={<ReviewForm />} />
         <Route path="/examples/components/" element={<Components />} />
       </Routes>
     </div>
