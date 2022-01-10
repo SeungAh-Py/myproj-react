638027089452c2a5bf372eddd7e0f7830b24d55e
SeungAh Kim <cpuasdnwin@naver.com>
2022-01-10 11:16:33 +0900

    review create 구현

--- src/pages/reviews/ReviewForm.jsx
+++ src/pages/reviews/ReviewForm.jsx
@@ -1,16 +1,27 @@
-import axios from 'axios';
+import Axios from 'axios';
 import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import DebugState from 'components/DebugStates';
 
 function ReviewForm() {
   const { reviewId } = useParams();
-  const [review, setReview] = useState(null);
-  const [error, setError] = useState(null);
-
+  const { Navigate } = useNavigate();
   const [fieldValues, setFieldValues] = useState({
     content: '',
     score: 5,
   });
+
+  const saveReview = async () => {
+    const url = 'http://127.0.0.1:8000/shop/api/reviews/';
+    try {
+      const response = await Axios.post(url, fieldValues);
+      console.group('saveRevie');
+      console.log(response.data);
+      console.group();
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -23,39 +34,25 @@ function ReviewForm() {
     });
   };
 
-  const navigate = useNavigate();
-
-  const handleSubmit = () => {
-    const url = 'http://localhost:8000/shop/api/reviews/';
-    axios
-      .post(url, fieldValues)
-      .then(() => {
-        navigate('/reviews/');
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => setFieldValues({}));
-  };
-
-  useEffect(() => {
-    if (reviewId) {
-      const url = `http://localhost:8000/shop/api/reviews/${reviewId}`;
-      axios
-        .get(url)
-        .then(({ data }) => setReview(data))
-        .catch((error) => setError(error));
-    } else {
-      setReview(null);
-    }
-  }, [reviewId]);
-
   return (
     <div>
       <h2>ReviewForm</h2>
       <br />
 
       <div>
+        <div>
+          <h2>
+            ReviewForm
+            {reviewId ? '수정' : '생성'}
+          </h2>
+          <DebugState reviewId={reviewId} />
+          <ReviewForm
+            fieldValues={fieldValues}
+            handleFieldChange={handleFieldChange}
+            handleSubmit={handleSubmit}
+          />
+        </div>
+
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2">
             평점
@@ -88,10 +85,7 @@ function ReviewForm() {
         </div>
 
         <div className="mb-4">
-          <button
-            className="shadow border bg-red-100 hover:bg-blue-300 border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight"
-            onClick={handleSubmit}
-          >
+          <button className="shadow border bg-red-100 hover:bg-blue-300 border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight">
             저장하기
           </button>
         </div>
--- src/pages/reviews/ReviewList.jsx
+++ src/pages/reviews/ReviewList.jsx
@@ -1,27 +1,22 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import Axios from 'axios';
 import DebugStates from 'components/DebugStates';
 import Review from 'components/Review';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
 function PageReviewList() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [reviewList, setReviewList] = useState([]);
   const navigate = useNavigate();
-
   useEffect(() => {
     refetch();
   }, []);
-
   const refetch = () => {
     setLoading(true);
     setError(null);
-
     const url = 'http://localhost:8000/shop/api/reviews/';
     // Promise 객체
-    axios
-      .get(url)
+    Axios.get(url)
       .then(({ data }) => {
         setReviewList(data);
       })
@@ -33,30 +28,19 @@ function PageReviewList() {
         setLoading(false);
       });
   };
-
   const deleteReview = (deletingReview) => {
     const { id: deletingReviewId } = deletingReview;
     const url = `http://localhost:8000/shop/api/reviews/${deletingReviewId}/`;
-
     setLoading(true);
     setError(null);
-
-    axios
-      .delete(url)
+    Axios.delete(url)
       .then(() => {
         console.log('삭제 성공');
-        // 선택지 1) 삭제된 항목만 상탯값에서 제거
-        // setReviewList((prevReviewList) => {
-        //   return prevReviewList.filter((review) => {
-        //     return review.id !== deletingReviewId;
-        //   });
-        // });
-
-        // return이 빠진 에로우 함수
+        // 선택지 #1) 삭제된 항목만 상탯값에서 제거
         setReviewList((prevReviewList) =>
           prevReviewList.filter((review) => review.id !== deletingReviewId),
         );
-        // 선택지 2) 전체를 새로고침
+        // 선택지 #2) 전체를 새로고침
       })
       .catch((error) => {
         setError(error);
@@ -65,42 +49,36 @@ function PageReviewList() {
         setLoading(false);
       });
   };
-
   return (
     <div>
       <h2>Review List</h2>
-
-      {loading && <div>Loading ... </div>}
+      {loading && <div>Loading ...</div>}
       {error && <div>통신 중에 오류가 발생했습니다.</div>}
-
       <button
         onClick={() => refetch()}
         className="bg-yellow-400 hover:bg-red-400 mr-1"
       >
         새로고침
       </button>
-
       <button
         onClick={() => navigate('/reviews/new/')}
         className="bg-blue-400 hover:bg-slate-400"
       >
         새 리뷰
       </button>
-
-      <div>
+      <div className="">
         {reviewList.map((review) => (
           <Review
             key={review.id}
             review={review}
+            handleEdit={() => navigate(`/reviews/${review.id}/edit/`)}
             handleDelete={() => deleteReview(review)}
           />
         ))}
       </div>
-
       <hr />
       <DebugStates loading={loading} error={error} reviewList={reviewList} />
     </div>
   );
 }
-
 export default PageReviewList;
